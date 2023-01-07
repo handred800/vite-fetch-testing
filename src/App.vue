cb72603eb6bee8f8118577eafc16ba86134c1ddd
handred800 <handred800@gmail.com>
2023-01-07 18:35:54 +0800

    更新 fetch

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <div></div>
   <HelloWorld />
 </template>
 <style>
-  html, body {
-    margin: 0;
-    padding: 5px;
-  }
-  
-  a {
-    text-decoration: none;
-    color: initial;
-  }
-  h1,h2,h3,h4,h5 {
-    margin: 0;
-  }
+html,
+body {
+  margin: 0;
+  padding: 5px;
+}
+
+a {
+  text-decoration: none;
+  color: initial;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+p {
+  margin: 0;
+}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,14 @@
 <script setup>
-import { ref, reactive } from 'vue'
+import { ref, reactive } from "vue";
 
-let id = ref('handred800');
+let id = ref("handred800");
 let resultList = reactive([]);
+let isLoading = ref(false);
 
 const urlsCreator = (length) => {
-  const url = `${import.meta.env.VITE_API_ENDPOINT}/page?owner=${id.value}&page=`;
+  const url = `${import.meta.env.VITE_API_ENDPOINT}/page?owner=${
+    id.value
+  }&page=`;
 
   let count = 0;
   return Array.from({ length }, () => {
@@ -17,44 +20,80 @@ const urlsCreator = (length) => {
 const gm = (() => {
   let count = 0;
 
-  function queueChecker({ data }) {
-    const list = data.list.map(({ csn, title, ctime }) => ({
-      id: csn,
-      title,
-      createAt: ctime,
-    }));
+  function queueChecker(data) {
+    const list = data.list.map(
+      ({ csn, title, coverpic, ctime, visit, gp, bahacoin, flagicon }) => ({
+        id: csn,
+        title,
+        image: coverpic,
+        createAt: ctime,
+        stats: {
+          view: Number(visit),
+          gp: Number(gp),
+          coin: Number(bahacoin.replaceAll(",", "")),
+        },
+      })
+    );
     console.log(list);
     resultList.push(...list);
   }
 
+  // 使用 gm 回傳
   return (url) => {
-    count += 1;
-    setTimeout(() => {
-      return fetch(url)
-        .then(res => res.json())
-        .then(queueChecker);
-    }, 1200 * count)
-  }
-})()
+    return new Promise((resolve) => {
+      count += 1;
+      setTimeout(() => {
+        fetch(url)
+          .then((res) => res.json())
+          .then(({ data }) => {
+            queueChecker(data);
+            resolve();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, 1000 * count);
+    });
+  };
+})();
 
 const fetchData = () => {
+  isLoading.value = true;
   resultList.splice(0);
   const urls = urlsCreator(5);
-  console.log(urls);
-  urls.forEach((url) => gm(url));
-}
-
+  const requests = urls.map((url) => gm(url));
+  Promise.all(requests).then(() => {
+    console.log("all fetches has sended");
+    isLoading.value = false;
+  });
+};
 </script>
 
 <template>
   <form @submit.prevent="fetchData">
-    <input type="text" v-model="id">
-    <button type="submit">查詢</button>
+    <input type="text" v-model="id" :readonly="isLoading" />
+    <button type="submit" :disabled="isLoading">
+      {{ isLoading ? "loading..." : "GO!" }}
+    </button>
   </form>
-  <a v-for="res in resultList" :href="`https://home.gamer.com.tw/artwork.php?sn=${res.id}`" class="card"
-    target="_blank">
+  <a
+    v-for="res in resultList"
+    :href="`https://home.gamer.com.tw/artwork.php?sn=${res.id}`"
+    class="card"
+    target="_blank"
+    :key="res.id"
+  >
     <h4>{{ res.title }}</h4>
-    <small>{{ res.createAt }}</small>
+    <p>{{ res.createAt }}</p>
+    <div style="display: flex">
+      <img :src="res.image" alt="" style="max-width: 100px" />
+      <ul>
+        <li>view：{{ res.stats.view }}</li>
+        <li>gp：{{ res.stats.gp }}</li>
+        <li>coin：{{ res.stats.coin }}</li>
+      </ul>
+    </div>
+
     <!-- <pre>{{ res }}</pre> -->
   </a>
 </template>
@@ -66,12 +105,13 @@ const fetchData = () => {
   width: 100%;
   padding: 15px 20px;
   margin-bottom: 15px;
-  box-shadow: 0 3px 5px rgba(0, 0, 0, .15);
+  background: #fff;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.15);
   box-sizing: border-box;
-  transition: .15s;
+  transition: 0.15s;
 }
 .card:hover {
-  box-shadow: 0 3px 15px rgba(0, 0, 0, .15);
+  box-shadow: 0 3px 15px rgba(0, 0, 0, 0.15);
   transform: translateY(-2px);
 }
 
