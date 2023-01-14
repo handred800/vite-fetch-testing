428071e112bcb03423da793bfe50b925157dacde
handred800 <handred800@gmail.com>
2023-01-14 14:59:04 +0800

    新增 排行功能初步完成

--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -7,33 +7,39 @@
 </script>
 
 <template>
-  <a
-    v-for="article in articles"
-    :href="`https://home.gamer.com.tw/artwork.php?sn=${article.id}`"
-    class="card"
-    target="_blank"
-    :key="article.id"
-  >
-    <h4>{{ article.title }}</h4>
-    <p>{{ article.createAt }}</p>
-    <div style="display: flex">
-      <img :src="article.image" alt="" style="max-width: 100px" />
-      <ul>
-        <li>view：{{ article.stats.view }}</li>
-        <li>gp：{{ article.stats.gp }}</li>
-        <li>coin：{{ article.stats.coin }}</li>
-      </ul>
-    </div>
-
-    <!-- <pre>{{ res }}</pre> -->
-  </a>
+  <div class="card-list">
+    <a
+      v-for="article in articles"
+      :href="`https://home.gamer.com.tw/artwork.php?sn=${article.id}`"
+      class="card"
+      target="_blank"
+      :key="article.id"
+    >
+      <h4>{{ article.title }}</h4>
+      <p>{{ article.createAt }}</p>
+      <div style="display: flex">
+        <img :src="article.image" alt="" style="max-width: 100px" />
+        <ul>
+          <li>view：{{ article.stats.view }}</li>
+          <li>gp：{{ article.stats.gp }}</li>
+          <li>coin：{{ article.stats.coin }}</li>
+        </ul>
+      </div>
+  
+      <!-- <pre>{{ res }}</pre> -->
+    </a>
+  </div>
 </template>
 
 <style scoped>
+
+.card-list {
+  display: flex;
+  flex-wrap: wrap;
+}
 .card {
   display: block;
-  max-width: 500px;
-  width: 100%;
+  flex: 1 0 25%;
   padding: 15px 20px;
   margin-bottom: 15px;
   background: #fff;
@@ -44,6 +50,13 @@
 .card:hover {
   box-shadow: 0 3px 15px rgba(0, 0, 0, 0.15);
   transform: translateY(-2px);
+}
+
+.card img {
+  width: 100px;
+  height: 100px;
+  object-fit: contain;
+  background-color: silver;
 }
 
 pre {
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -17,60 +17,141 @@
     return pipe(options)
   }) 
 
-  const filtedArticles = computed(() => {
-    if (filterYear.value === '') return articles.value
-    return _.filter(articles.value, (article) => article.createAt.startsWith(filterYear.value));
-  });
+  // 數據type select
+  const filterStats = ref('createAt');
+
+  // filter
+  function filterWith(list) {
+    return _.filter(list, (item) => item.createAt.startsWith(filterYear.value))
+  }
+  // order
+  function orderWith(list) {
+    return _.orderBy(list, [filterStats.value], ['desc'])
+  }
+
+  const filterPipe = _.flow([filterWith, orderWith]);
+  const filtedArticles = computed(() => filterPipe(articles.value));
+ 
+  // 加總數據
   const totalStats = (statsName) => _.reduce(filtedArticles.value, (sum, current) => sum + current.stats[statsName], 0);
   
 </script>
 
 <template>
-  <select v-model="filterYear">
-    <option value="">全部</option>
-    <option :value="option" v-for="option in yearOptions">{{ option }}</option>
-  </select>
-  <p>共 {{articles.length}} 篇文章、{{ totalStats('view') }} 觀看數、{{ totalStats('gp') }} GP</p>
-  <a
-    v-for="article in filtedArticles"
-    :href="`https://home.gamer.com.tw/artwork.php?sn=${article.id}`"
-    class="card"
-    target="_blank"
-    :key="article.id"
-  >
-    <h4>{{ article.title }}</h4>
-    <p>{{ article.createAt }}</p>
-    <div style="display: flex">
-      <img :src="article.image" alt="" style="max-width: 100px" />
+  <fieldset>
+    <legend>控制項</legend>
+    年度: 
+    <select v-model="filterYear">
+      <option value="">全部</option>
+      <option :value="option" v-for="option in yearOptions">{{ option }}</option>
+    </select>
+    排序:
+    <select v-model="filterStats">
+      <option value="createAt">發佈日期</option>
+      <option value="stats.view">觀看數</option>
+      <option value="stats.gp">GP</option>
+      <option value="stats.coin">巴幣</option>
+    </select>
+  </fieldset>
+  <p>共 {{filtedArticles.length}} 篇文章
+    、{{ totalStats('view') }} 觀看數
+    、{{ totalStats('gp') }} GP
+    、{{ totalStats('coin') }} 巴幣
+  </p>
+  <details open>
+    <summary>排行</summary>
+    <div class="template">
       <ul>
-        <li>view：{{ article.stats.view }}</li>
-        <li>gp：{{ article.stats.gp }}</li>
-        <li>coin：{{ article.stats.coin }}</li>
+        <li v-for="(article, index) in filtedArticles" :key="article.id">
+          <div style="display: flex;">
+            <span style="margin-right: 10px">{{ index+1 }}</span>
+            <a target="_blank" :href="`https://home.gamer.com.tw/artwork.php?sn=${article.id}`">{{article.title}}</a>
+          </div>
+          {{ _.get(article, filterStats) }}
+        </li>
       </ul>
+      <!-- <textarea readonly>
+        {{ filtedArticles }}
+      </textarea> -->
     </div>
-
-    <!-- <pre>{{ res }}</pre> -->
-  </a>
+  </details>
+  <!-- <div class="card-list">
+    <a
+      v-for="article in filtedArticles"
+      :href="`https://home.gamer.com.tw/artwork.php?sn=${article.id}`"
+      class="card card-sm"
+      style="margin-right: 15px"
+      target="_blank"
+      :key="article.id"
+    >
+      <h4>{{ article.title }}</h4>
+      <p>{{ article.createAt }}</p>
+      <div style="display: flex">
+        <img :src="article.image" alt="" />
+        <ul>
+          <li>view：{{ article.stats.view }}</li>
+          <li>gp：{{ article.stats.gp }}</li>
+          <li>coin：{{ article.stats.coin }}</li>
+        </ul>
+      </div>
+    </a>
+  </div> -->
 </template>
 
 <style scoped>
+.card-list {
+  display: flex;
+  overflow-x: auto;
+}
+.card-list .card {
+  flex: 0 0 300px;
+}
 .card {
   display: block;
   max-width: 500px;
   width: 100%;
   padding: 15px 20px;
-  margin-bottom: 15px;
   background: #fff;
   box-shadow: 0 3px 5px rgba(0, 0, 0, 0.15);
   box-sizing: border-box;
   transition: 0.15s;
 }
+.card-sm {
+  max-width: 300px;
+  padding: 5px 10px;
+}
 .card:hover {
   box-shadow: 0 3px 15px rgba(0, 0, 0, 0.15);
   transform: translateY(-2px);
 }
+.card img {
+  width: 100px;
+  height: 100px;
+  object-fit: contain;
+  background-color: silver;
+}
 
-pre {
-  white-space: pre-line;
+textarea {
+  display: block;
+  width: 100%;
+  resize: vertical;
+  /* white-space: pre-line; */
+}
+
+.template {
+  width: 50%;
+  max-height: 500px;
+  overflow-y: auto;
+}
+.template ul {
+  padding: 0 15px;
+}
+.template li {
+  display: flex;
+  justify-content: space-between;
+}
+.template a {
+  display: inline-block;
+  color: green;
 }
 </style>
