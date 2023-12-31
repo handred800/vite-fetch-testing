4f9477186f53b6fadcfd4404ea74ced24c6d8dd9
handred800 <handred800@gmail.com>
2024-01-01 00:30:28 +0800

    新增 列表分段顯示(article)

--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -1,16 +1,23 @@
 <script setup>
+  import { ref, computed } from 'vue';
   import { storeToRefs } from 'pinia';
   import { useArticlesStore } from '../store';
   import ArticleCard from '../components/ArticleCard.vue';
 
   const articleStore = useArticlesStore();
   const { articles } = storeToRefs(articleStore);
+
+  const currentTotal = ref(50);
+
+  const articlesIsShow = computed(() => articles.value.slice(1, currentTotal.value));
+
 </script>
 
 <template>
   <div class="grid grid-equalHeight">
-    <div class="col-3_xs-12_sm-6_md-4" v-for="article in articles">
+    <div class="col-3_xs-12_sm-6_md-4" v-for="article in articlesIsShow">
       <article-card :article="article"></article-card>
     </div>
+    <button v-if="currentTotal < articles.length" @click="currentTotal += 30">more</button>
   </div>
 </template>
